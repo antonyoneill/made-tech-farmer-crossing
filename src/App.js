ec8df3e4a02f69287e9338a61d42abb4fc4872a0
Antony O'Neill <antony@ant-web.co.uk>
2020-04-08 17:04:50 +0100

    Add ability to change number of corn & calulate correct cost

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import calculateCost from './utils/calculateCost'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [numberOfCorn, setNumberOfCorn] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         🌽 Corn Trip Calculator
       </header>
       <section className="App-section">
-        Cost of trip with one corn: <span>25p</span>
+        <section>
+        <label>Number of corn
+          <input
+            type="number"
+            onChange={(event) => {
+              const newCountOfCorn = parseInt(event.currentTarget.value,10)
+              if (!isNaN(newCountOfCorn)) {
+                setNumberOfCorn(newCountOfCorn)
+              }
+            }} 
+            value={numberOfCorn}
+          />
+        </label>
+        </section>
+        <section className="App-section">
+        Cost of trip with {numberOfCorn} corn: <span>{calculateCost(numberOfCorn)}p</span>
+        </section>
       </section>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('it shows the cost for one corn', () => {
+test.each([[0,0],[1,25],[10,19 * 25]])('the user can input the number corn and be told the cost of transport', (numberOfCorn, expectedCost) => {
   const context = render(<App />);
-  const cost = context.getByText('25p');
+
+  const input = context.getByLabelText("Number of corn")
+
+  fireEvent.change(input, {target:{value: numberOfCorn}})
+
+  const cost = context.getByText(`${expectedCost}p`)
   expect(cost).toBeInTheDocument();
-});
+})
+test('the user does not see NaN when the input is blank', () => {
+  const context = render(<App />);
+
+  const input = context.getByLabelText("Number of corn")
+
+  fireEvent.change(input, {target:{value: ""}})
+
+  const cost = context.queryByText(`NaNp`)
+  expect(cost).not.toBeInTheDocument();
+})
