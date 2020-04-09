a5d496e0c52cb70d6402116853dead6883189ec4
Antony O'Neill <antony@ant-web.co.uk>
2020-04-09 11:30:39 +0100

    Updates

---
+++ precache-manifest.b48343f2860fb965eebedce59acccedd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725fdbecd203cd9ac3eaf7a782223546",
+    "url": "/made-tech-farmer-crossing/index.html"
+  },
+  {
+    "revision": "8866009598db82c4af8c",
+    "url": "/made-tech-farmer-crossing/static/css/main.4cb7ab91.chunk.css"
+  },
+  {
+    "revision": "5ccb189c4d9d0ccceda4",
+    "url": "/made-tech-farmer-crossing/static/js/2.a462a9d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/made-tech-farmer-crossing/static/js/2.a462a9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8866009598db82c4af8c",
+    "url": "/made-tech-farmer-crossing/static/js/main.e420fd41.chunk.js"
+  },
+  {
+    "revision": "9f4167d50f9ded6033fd",
+    "url": "/made-tech-farmer-crossing/static/js/runtime-main.5cee864c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/made-tech-farmer-crossing/static/media/logo.5d5d9eef.svg"
+  }
+]);
