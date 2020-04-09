f7689e0cb5733b30df6c2a25fcd8f85b6ef74f5e
Antony O'Neill <antony@ant-web.co.uk>
2020-04-09 11:19:00 +0100

    Render £0.00 when input is blank

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test.each([[0,"£0.00"],[1,"£0.25"],[10,"£4.75"]])('the user can input the number corn and be told the cost of transport', (numberOfCorn, expectedCost) => {
+test.each([["", "£0.00"], [0,"£0.00"],[1,"£0.25"],[10,"£4.75"]])('the user can input the number corn and be told the cost of transport', async (numberOfCorn, expectedCost) => {
   const context = render(<App />);
 
   const input = context.getByLabelText("Number of corn")
@@ -11,14 +11,4 @@ test.each([[0,"£0.00"],[1,"£0.25"],[10,"£4.75"]])('the user can input the num
 
   const cost = context.getByText(expectedCost)
   expect(cost).toBeInTheDocument();
-})
-test('the user does not see NaN when the input is blank', () => {
-  const context = render(<App />);
-
-  const input = context.getByLabelText("Number of corn")
-
-  fireEvent.change(input, {target:{value: ""}})
-
-  const cost = context.queryByText(/NaN/)
-  expect(cost).not.toBeInTheDocument();
 })
